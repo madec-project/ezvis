0f580ef0a7d4553a8a68f5665a7c4e2013dfe95b
Nicolas Thouvenin <nthouvenin@gmail.com>
2014-09-18 14:06:55 +0200

    first import

---
+++ assets/js/sb-admin-2.js
@@ -0,0 +1,46 @@
+function bytesToSize(b) {
+  var tb = ((1 << 30) * 1024), gb = 1 << 30, mb = 1 << 20, kb = 1 << 10, abs = Math.abs(b);
+  if (abs >= tb) return (Math.round(b / tb * 100) / 100) + 'tb';
+  if (abs >= gb) return (Math.round(b / gb * 100) / 100) + 'gb';
+  if (abs >= mb) return (Math.round(b / mb * 100) / 100) + 'mb';
+  if (abs >= kb) return (Math.round(b / kb * 100) / 100) + 'kb';
+  return b + 'b';
+}
+
+$(function() {
+
+    $('#side-menu').metisMenu();
+
+    $("div[data-url]").each(function(e) {
+      var self = this;
+      $.getJSON($(self).data('url'), function(obj) {
+        var func = new Function("data", "return " + $(self).data('return'));
+        $(self).html(func(obj.data));
+      })
+    });
+});
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function() {
+    $(window).bind("load resize", function() {
+        topOffset = 50;
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse')
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse')
+        }
+
+        height = (this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    })
+  })
+
+
