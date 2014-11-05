484265d21dcb64da236d25e08ca99be48b9fbf35
Nicolas Thouvenin <nthouvenin@gmail.com>
2014-11-05 17:42:23 +0100

    transform theme to a standalone application

---
+++ server.js
@@ -0,0 +1,11 @@
+/*jshint node:true, laxcomma:true*/
+"use strict";
+
+module.exports = function(config, run) {
+  config.set('theme', __dirname);
+  run();
+}
+
+if (!module.parent) {
+  require('castor-core')(module.exports);
+}
