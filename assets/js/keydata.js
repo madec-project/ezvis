db97bf235b316377526e0af1eb94e65033d36f16
François Parmentier <francois.parmentier@gmail.com>
2015-02-03 15:27:28 +0100

    Adapt metrics to new JBJ syntax ($)

--- assets/js/keydata.js
+++ assets/js/keydata.js
@@ -36,11 +36,15 @@ $(document).ready(function () {
       .get('/corpus.json?l=1&columns[0][data]=computedDate&columns[0][orderable]=true&order[0][column]=0&order[0][dir]=desc')
       .end(function(res) {
         var fields = Object.keys(res.body.data[0]), i = 0;
+        console.log("fields", fields);
+        console.log("Config", Config.corpusFields);
         Object.keys(Config.corpusFields).forEach(function(item) {
-          if (fields.indexOf(item) !== -1 && Config.corpusFields[item].visible) {
-            self.keydata[i].label = Config.corpusFields[item].label;
+          item = item.slice(1);
+          console.log("item",item);
+          if (fields.indexOf(item) !== -1 && Config.corpusFields['$'+item].visible) {
+            self.keydata[i].label = Config.corpusFields['$'+item].label;
             self.keydata[i].value = res.body.data[0][item];
-            self.keydata[i].icon = Config.corpusFields[item].icon || 'dollar';
+            self.keydata[i].icon = Config.corpusFields['$'+item].icon || 'dollar';
             i++;
           }
         });
