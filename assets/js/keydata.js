5ac1eef42de757267a734b2d4db9469370b85415
François Parmentier <francois.parmentier@gmail.com>
2015-02-03 15:34:11 +0100

    Clean traces.

--- assets/js/keydata.js
+++ assets/js/keydata.js
@@ -36,11 +36,8 @@ $(document).ready(function () {
       .get('/corpus.json?l=1&columns[0][data]=computedDate&columns[0][orderable]=true&order[0][column]=0&order[0][dir]=desc')
       .end(function(res) {
         var fields = Object.keys(res.body.data[0]), i = 0;
-        console.log("fields", fields);
-        console.log("Config", Config.corpusFields);
         Object.keys(Config.corpusFields).forEach(function(item) {
           item = item.slice(1);
-          console.log("item",item);
           if (fields.indexOf(item) !== -1 && Config.corpusFields['$'+item].visible) {
             self.keydata[i].label = Config.corpusFields['$'+item].label;
             self.keydata[i].value = res.body.data[0][item];
