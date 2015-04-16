898efa882f311623ab54f4c71c1386fb32d094e4
François Parmentier <francois.parmentier@gmail.com>
2015-04-16 15:47:18 +0200

    Split test in two parts.

--- test/test2.js
+++ test/test2.js
@@ -17,12 +17,21 @@ module.exports = {
     .open('http://localhost:'+conf.port+'/documents.html')
     .waitFor(nbRecordsPresent)
     .assert.exists('div#dataTables-documents_info.dataTables_info', '"Showing 1 to 4 of 4 entries"')
-    .assert.text('div#dataTables-documents_info.dataTables_info', "Showing 1 to 4 of 4 entries", '"Showing 1 to 4 of 4 entries"')
-    .assert.text('#dataTables-documents tbody tr:first-child td:first-child a', '1906','"1906" in first line')
-    .assert.text('#dataTables-documents tbody tr:first-child td:nth-child(2) a', 'Kurt Gödel','"Kurt Gödel" in first line')
-    .click('#dataTables-documents tbody tr:nth-child(4) td:first:first-child a')
-    .waitForElement('#documents tbody tr:first-child')
-    .assert.numberOfElements('#documents tbody tr', 3, '3 fields displayed')
+    .assert.text('div#dataTables-documents_info.dataTables_info').is("Showing 1 to 4 of 4 entries", '"Showing 1 to 4 of 4 entries"')
+    .assert.text('#dataTables-documents tr:first-child td:first-child a').is('1906','"1906" in first line')
+    .assert.text('#dataTables-documents tr:first-child td:nth-child(2) a').is('Kurt Gödel','"Kurt Gödel" in first line')
+    .click('#dataTables-documents tr:nth-child(4) td:first:first-child a')
+    .assert.title().is('Test 2 - Display document','Correct')
+    // .assert.numberOfElements('#documents tbody tr').is(3, '3 fields displayed')
+    // .assert.text('#documents tbody tr:first-child td:nth-child(2)').is('Douglas Hofstadter', 'Name is "Douglas Hofstadter"')
+    .done();
+  },
+  "Good display": function (test) {
+    test
+    .open('http://localhost:'+conf.port+'/display/Z5OAoW.html')
+    .assert.title().is('Test 2 - Display document','Correct')
+    .assert.numberOfElements('#documents tbody tr').is(3, '3 fields displayed')
+    .assert.text('#documents tbody tr:first-child td:nth-child(2)').is('Douglas Hofstadter', 'Name is "Douglas Hofstadter"')
     .done();
   }
 };
