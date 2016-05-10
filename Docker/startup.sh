01ecb43a7fb08b4a9aba4d68eabf1345be5ff460
nlamblin <nicolas.lamblin@outlook.com>
2016-05-10 18:05:15 +0200

    upgrade mongodb connection uri for latest docker version

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -1,7 +1,2 @@
 # In association with Dockerfile
-if [[ -z "$MONGO_PORT_27017_TCP_ADDR" && -z "$MONGO_PORT_27017_TCP_PORT" ]]
-then
-	ezvis /root/data --port 3000
-else
-	ezvis /root/data --connexionURI "mongodb://$MONGO_PORT_27017_TCP_ADDR:$MONGO_PORT_27017_TCP_PORT/castor" --port 3000
-fi
+ezvis /root/data --connexionURI "mongodb://ezvis_db:27017/castor" --port 3000
