1063e3d071e140c1383261a38830609b97b1a6d4
Nicolas Lamblin <nicolas.lamblin@outlook.com>
2016-05-23 17:36:44 +0200

    Update EZMASTER_MONGODB_HOST_PORT

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -1,2 +1,2 @@
 # In association with Dockerfile
-ezvis /root/data --connexionURI "mongodb://${MONGODB_URI:-ezvis_db:27017}/castor" --port 3000
+ezvis /root/data --connexionURI "mongodb://${EZMASTER_MONGODB_HOST_PORT:-ezvis_db:27017}/castor" --port 3000
