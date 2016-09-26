03cf37e5fbeea157443b28ba05d19bdb0cea4cd2
Stéphane Gully <stephane.gully@gmail.com>
2016-09-26 10:06:35 +0200

    tell the ezvis docker image to use a specific ezmaster mongodb database for each instances (based on the technical name)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if [ "$MONGO_HOST_PORT" != "" ]; then
-  ./cli /app/example/data --connexionURI "mongodb://$MONGO_HOST_PORT/$MONGO_DATABASE" --port 3000
-else
-  ./cli /app/example/data --connexionURI "mongodb://${EZMASTER_MONGODB_HOST_PORT:-ezvis_db:27017}/ezvis" --port 3000
+if [ "$MONGO_HOST_PORT" == "" ]; then
+  MONGO_HOST_PORT=${EZMASTER_MONGODB_HOST_PORT:-ezvis_db:27017}
 fi
+if [ "$MONGO_DATABASE" == "" ]; then
+  MONGO_DATABASE=${EZMASTER_TECHNICAL_NAME:-ezvis}
+fi
+
+echo "Running ezvis connected to mongodb://$MONGO_HOST_PORT/$MONGO_DATABASE"
+./cli /app/example/data --connexionURI "mongodb://$MONGO_HOST_PORT/$MONGO_DATABASE" --port 3000
